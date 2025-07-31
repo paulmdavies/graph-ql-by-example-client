2264ef658b25588e4a99e7dd3ad3c98babf37185
Paul Davies <paulmdavies@gmail.com>
2025-07-31 16:46:24 +0100

    Course 5

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,19 +1,46 @@
 import { useParams } from 'react-router';
-import {useEffect, useState} from "react";
+import {use, useEffect, useState} from "react";
 import {getCompany} from "../lib/graphql/queries.js";
 import JobList from "../components/JobList.jsx";
 
 function CompanyPage() {
   const { companyId } = useParams();
 
-  const [company, setCompany] = useState()
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false
+  })
   useEffect(() => {
-    getCompany(companyId).then(setCompany)
+    (async() => {
+      try {
+        const company = await getCompany(companyId);
+        setState(
+          {
+            company,
+            loading: false,
+            error: false
+          }
+        )
+      } catch (error) {
+        setState(
+          {
+            company: null,
+            loading: false,
+            error: true
+          }
+        )
+      }
+    })();
   }, [companyId]);
 
-  console.log('[CompanyPage] company: ', company)
-  if (!company) {
+  console.log('[CompanyPage] state: ', state)
+  const { company, loading, error } = state;
+  if (loading) {
     return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Not found</div>
   }
   return (
     <div>
